a9020898ac269ab10cd8d5991b9fe6126f26865f
iiai22 <114829127+iiai22@users.noreply.github.com>
2023-07-25 19:31:05 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="text">
+        <h1 className="title">
+          <span>H</span>ello StackBlitz!
+        </h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    </div>
+  );
+}
